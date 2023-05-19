427bacae8959b68a6d11c4d1d65b40b3d972157c
Joris Remmers <J.J.C.Remmers@tue.nl>
2023-05-19 14:19:39 +0200

    WiP

---
+++ app/composite/__init__.py
@@ -0,0 +1,9 @@
+from .src.composite import (
+    TransverseIsotropic    ,
+    Layer                  ,
+    Laminate               ,
+    stressTransformation   ,
+    strainTransformation   ,
+    mixMaterials           ,    
+)
+
