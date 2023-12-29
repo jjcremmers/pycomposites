9efe9cf979ca2d497d6f0894054eef85b50d2e97
Remmers, J.J.C. <j.j.c.remmers@tue.nl>
2023-12-29 17:48:37 +0200

    Update file setup.py

--- pycomposites/setup.py
+++ pycomposites/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pycomposites",
-    version="1.0.1",
+    version="1.0.2",
     py_modules=['composite'],
     install_requires=[],
 )
