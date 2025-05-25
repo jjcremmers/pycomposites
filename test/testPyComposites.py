7d567d03e95bd60d794cf93e3ed39c8a5daec010
Joris Remmers <J.J.C.Remmers@tue.nl>
2025-05-25 22:02:58 +0200

    Addig unittest

---
+++ test/testPyComposites.py
@@ -0,0 +1,27 @@
+"""A set of classes and functions to read and transform hdf5 files in the NDF 
+    file format.
+
+  (c) Joris Remmers (2021-2023)
+  
+"""
+import unittest,os
+from pycomposites.composite import TransverseIsotropic,mixMaterials
+
+  
+class PyCompositeTesting(unittest.TestCase):
+        
+    def testMixMaterials(self): 
+    
+        carbon = TransverseIsotropic( 220e9,0.2,91.7e9)
+        epoxy  = TransverseIsotropic( 3.6e9,0.35,1.33e9)
+        
+        compmat = mixMaterials( carbon , epoxy , 0.6 )
+                
+        self.assertAlmostEqual( compmat.E1   , 1.3344e+11 , delta=1.0e8 )
+        self.assertAlmostEqual( compmat.E2   , 8.784e+09  , delta=1.0e7 )   
+        self.assertAlmostEqual( compmat.nu12 , 0.26       , delta=0.001 )
+        self.assertAlmostEqual( compmat.G12  , 3.254e+09  , delta=1.0e7 ) 
+    
+                
+if __name__ == '__main__':
+    unittest.main()
