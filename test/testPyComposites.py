738b94429dea95eb25e6ae92cae482bcf5975f1f
Joris Remmers <J.J.C.Remmers@tue.nl>
2025-09-26 09:16:19 +0200

    Adding extra test

--- test/testPyComposites.py
+++ test/testPyComposites.py
@@ -42,27 +42,32 @@ class PyCompositeTesting(unittest.TestCase):
         self.assertAlmostEqual( carbon.alpha2 , 3.0e-6 , delta=1.0e-12 )    
         
     def test_material_init_scalar(self):
+    
         mat = TransverseIsotropic(100e9, 0.25)
         self.assertEqual(mat.E1, 100e9)
         self.assertEqual(mat.E2, 100e9)
         self.assertAlmostEqual(mat.nu21, 0.25, delta=1e-6)
 
     def test_material_init_list(self):
+    
         mat = TransverseIsotropic([150e9, 10e9], 0.3)
         self.assertEqual(mat.E1, 150e9)
         self.assertEqual(mat.E2, 10e9)
 
     def test_invalid_E_list(self):
+    
         with self.assertRaises(ValueError):
             TransverseIsotropic([1,2,3], 0.3)
 
     def test_setAlpha(self):
+    
         mat = TransverseIsotropic(70e9, 0.2)
         mat.setAlpha([1e-6, 2e-6])
         self.assertEqual(mat.alpha1, 1e-6)
         self.assertEqual(mat.alpha2, 2e-6)
 
     def test_getQ_and_S_consistency(self):
+    
         mat = TransverseIsotropic([100e9, 10e9], 0.25, 5e9)
         Q = mat.getQ()
         S = mat.getS()
@@ -71,12 +76,14 @@ class PyCompositeTesting(unittest.TestCase):
         self.assertTrue(np.allclose(I, np.eye(3), atol=1e-6))
 
     def test_getQbar_rotation(self):
+    
         mat = TransverseIsotropic([100e9, 10e9], 0.25, 5e9)
         Q0 = mat.getQbar(0.0)
         Q90 = mat.getQbar(90.0)
         self.assertTrue(np.allclose(Q0[0,0], Q90[1,1], atol=1e-6))
 
     def test_getAlpha_rotation(self):
+    
         mat = TransverseIsotropic([100e9, 10e9], 0.25, 5e9, alpha=[1e-6, 2e-6])
         a0 = mat.getAlpha(0.0)
         a90 = mat.getAlpha(90.0)
@@ -84,6 +91,7 @@ class PyCompositeTesting(unittest.TestCase):
         self.assertAlmostEqual(a90[1], 1e-6)
 
     def test_laminate_ABD(self):
+    
         mat = TransverseIsotropic([100e9, 10e9], 0.25, 5e9)
         lam = Laminate()
         lam.addMaterial("m", mat)
@@ -95,6 +103,7 @@ class PyCompositeTesting(unittest.TestCase):
         self.assertTrue(np.allclose(B, np.zeros((3,3)), atol=1e-12))
 
     def test_elastic_effective(self):
+    
         mat = TransverseIsotropic([100e9, 10e9], 0.25, 5e9)
         lam = Laminate()
         lam.addMaterial("m", mat)
@@ -105,12 +114,14 @@ class PyCompositeTesting(unittest.TestCase):
         self.assertGreater(Ey, 1e9)
 
     def test_failure_max_stress(self):
+    
         mat = TransverseIsotropic(100e9, 0.25, 5e9)
         mat.setFailureProperties([1000, 800, 50, 40, 30])
         FI = mat.getFIMaximumStress(np.array([1000, 0, 0]))
         self.assertAlmostEqual(FI, 1.0)
 
     def test_stressTransformation(self):
+    
         sigma = np.array([1.0, 0.0, 0.0])
         sig90 = stressTransformation(sigma, 90)
         self.assertAlmostEqual(sig90[1], 1.0, places=6)
@@ -119,10 +130,6 @@ class PyCompositeTesting(unittest.TestCase):
     
         self.assertEqual(Macauley(5.0), 5.0)
         self.assertEqual(Macauley(-3.0), 0.0)
-
-if __name__ == "__main__":
-    unittest.main()
-             
-                
+                        
 if __name__ == '__main__':
     unittest.main()
